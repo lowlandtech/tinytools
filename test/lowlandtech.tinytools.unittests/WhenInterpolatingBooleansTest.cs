659e97b2b21bfaccd3fdbb0978963c521ce6b8f2
Wendell Muntslag van Amzink <wendellmva@lowlandtech.com>
2021-07-18 11:14:00 +0200

    refactor: change layout
    - changed namespaces
    - add test harness

---
+++ test/lowlandtech.tinytools.unittests/WhenInterpolatingBooleansTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LowlandTech.TinyTools.Test.Fakes;
+using Xunit;
+
+namespace LowlandTech.TinyTools.Tests
+{
+    public class WhenInterpolatingBooleansTest : WhenTestingFor<string>
+    {
+        private Person _person;
+        private string _result;
+
+        protected override string For()
+        {
+            return "Hello world, married: {IsMarried}";
+        }
+
+        protected override void Given()
+        {
+            _person = new Person
+            {
+                IsMarried = true
+            };
+        }
+
+        protected override void When()
+        {
+            _result = Sut.Interpolate(_person);
+        }
+
+        [Fact]
+        public void ItShouldInterpolateIsMarried()
+        {
+            _result.Should().Be("Hello world, married: True");
+        }
+    }
+}
